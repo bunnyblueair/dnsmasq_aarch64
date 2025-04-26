e3c52af65bf28944d5160c5e3b404d09a0c354ac
Hello <bunnyblue.air+github@gmail.com>
2025-04-26 11:41:57 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 cd dnsmasq.git 
 ver=$(./bld/get-version .)
 
-make COPTS="-DHAVE_IPSET -DHAVE_BROKEN_RTC -DHAVE_AUTH -DNO_TFTP -DNO_ID -DHAVE_LOOP -DNO_INOTIFY -DNO_DUMPFILE -DNO_GMP -DHAVE_DNSSEC " CFLAGS="-I/usr/local/include -DVERSION=\\\"$ver\\\""
+make COPTS="-DHAVE_IPSET -DHAVE_BROKEN_RTC -DHAVE_AUTH -DNO_TFTP -DNO_ID -DHAVE_LOOP -DNO_INOTIFY -DNO_DUMPFILE -DNO_GMP -DHAVE_DNSSEC" \
+     CFLAGS="-I/usr/local/include -DVERSION=\\\"$ver\\\""
+
 
 cd src
 aarch64-linux-gnu-gcc  -o dnsmasq cache.o rfc1035.o util.o option.o forward.o network.o dnsmasq.o dhcp.o lease.o rfc2131.o netlink.o dbus.o bpf.o helper.o tftp.o log.o conntrack.o dhcp6.o rfc3315.o dhcp-common.o outpacket.o radv.o slaac.o auth.o ipset.o pattern.o domain.o dnssec.o blockdata.o tables.o loop.o inotify.o poll.o rrfilter.o edns0.o arp.o crypto.o dump.o ubus.o metrics.o domain-match.o nftset.o   /usr/local/lib/libhogweed.a  /usr/local/lib/libnettle.a 
